195d5cb89fd54729ce27af3605563653922f5664
Nithinbharathi <106980567+Nithinbharathi93@users.noreply.github.com>
2024-03-22 15:10:39 +0000

    asdf

---
+++ code.js
@@ -0,0 +1,22 @@
+// window.onload = function() {
+//     document.getElementById('myOverlay').style.display = 'block';
+//     document.getElementById('img01').src = document.getElementById('myImg').src;
+//     document.getElementById('caption').innerHTML = document.getElementById('myImg').alt;
+//     document.getElementById('popupText').innerHTML = "This is some text in the lightbox popup.";
+// };
+
+// function closeOverlay() {
+//     document.getElementById('myOverlay').style.display = 'none';
+// }
+// let prevPageUrl = "http://127.0.0.1:5500/try.html";
+//   document.addEventListener("DOMContentLoaded", function() {
+    
+//     document.getElementById("getCode").addEventListener("click", function() {
+//         const deadDiv = document.getElementById("alrt");
+//         deadDiv.parentNode.removeChild(deadDiv);  // Remove the div from the DOM
+//       });
+//       document.getElementById("cancelCode").addEventListener("click", function() {
+//         window.location.href = "try.html";
+//       });
+//   });
+  
